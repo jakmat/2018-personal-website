f90bc595ee16d2648871d11b0f2b79bbc7935273
jakmat <jakubmatusiak87@gmail.com>
2018-08-23 23:20:48 +0200

    [DEV-1] Test commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const sayHello = () => {
+    console.log('Boilerplate!');
     /*eslint-disable no-console */
     console.log("Allo! We are all set!");
     console.log("Arrow functions are working");
