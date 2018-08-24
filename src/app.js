bc3d8936ec1f69439e4e0c2d333ea5a609026bbf
jakmat <jakubmatusiak87@gmail.com>
2018-08-24 23:37:22 +0200

    [DEV-2] Add descriptions and copyrights

--- src/app.js
+++ src/app.js
@@ -1,8 +1,1 @@
-const sayHello = () => {
-    console.log('Boilerplate!');
-    /*eslint-disable no-console */
-    console.log("Allo! We are all set!");
-    console.log("Arrow functions are working");
-};
-
-sayHello();
+console.log('app');
