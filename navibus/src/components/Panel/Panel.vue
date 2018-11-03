8494948e57f9821a6d23a0db350eb01596be8778
jakmat <jakubmatusiak87@gmail.com>
2018-11-03 18:02:14 +0100

    [DEV-5] Add Map and Panel components

---
+++ navibus/src/components/Panel/Panel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="panel"></div>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+}
+</script>
+
+<style lang="less" scoped>
+.panel {
+  background-color: #252525;
+  width: 20vw;
+  height: 100%;
+}
+</style>
---
+++ navibus/src/map/Map.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+}
+</script>
+
+<style lang="less" scoped>
+.map {
+  background-color: #202020;
+  width: 80vw;
+  height: 100%;
+}
+</style>
