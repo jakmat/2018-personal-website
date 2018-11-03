b34867ffdd20fbb06bd79e43154c2ed215c4715d
jakmat <jakubmatusiak87@gmail.com>
2018-11-03 19:56:14 +0100

    [DEV-6] Add leaflet map

--- navibus/src/main.js
+++ navibus/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import leaflet from 'leaflet';
 
 Vue.config.productionTip = false
+
+Vue.use(leaflet);
 
 new Vue({
   render: h => h(App),
--- navibus/src/map/Map.vue
+++ navibus/src/map/Map.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="map"></div>
+  <div class="map">
+    <div id="leaflet"></div>
+  </div>
 </template>
 
 <script>
+import leaflet from 'leaflet'; 
+
 export default {
   name: 'Map',
+
+  data() {
+    return {
+      map: null
+    };
+  },
+
+  mounted() {
+    this.map = leaflet.map('leaflet').setView([51.505, -0.09], 13);
+    // this.addMap();
+    console.log(this.map);
+
+  },
+
+  methods: {
+    addMap() {
+      leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiamFrbWF0IiwiYSI6ImNqbzFzaDBjcjBkcGgza29hbmU4dWV2NzcifQ.60inihwM9A5HEuK0pHvtIQ'
+      }).addTo(this.map);
+    }
+  }
 }
 </script>
 
@@ -13,5 +41,11 @@ export default {
   background-color: #202020;
   width: 80vw;
   height: 100%;
+  
+  #leaflet {
+    width: 500px;
+    height: 250px;
+
+  }
 }
 </style>
