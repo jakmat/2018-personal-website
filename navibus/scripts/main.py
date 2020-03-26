afeec80a791e0b93df682da62958cf736fff1fbc
jakmat <jakubmatusiak87@gmail.com>
2020-03-26 17:03:05 +0100

    Separate input from output, add abstractions

--- navibus/scripts/main.py
+++ navibus/scripts/main.py
@@ -2,54 +2,70 @@ from skyfield.api import load, Topos
 from string import Template
 from pytz import timezone
 from datetime import datetime
-planets = load('de421.bsp')
-timescale = load.timescale()
 
-def get_astrometric(celestial_obj, observer_pos, observer_time):
-    return observer_pos.at(observer_time).observe(celestial_obj).apparent()
+class Celestial:
+    def __init__(self, name):
+        self.name = name
+        self.caption = name.capitalize()
+        self.set_object()
 
-def get_lodz_time():
-    central = timezone('Europe/Warsaw')
-    d = datetime(2014, 1, 16, 1, 32, 9)
-    c = central.localize(d)
-    t = timescale.utc(c)
-    # UTC datetime
-    dt = t.utc_datetime()
-    print('UTC: ' + str(dt))
-    # Converting back to an Eastern Time datetime.
-    dt = t.astimezone(central)
-    print('EST: ' + str(dt))
-    return t
+    def set_object(self):
+        planets = load('de421.bsp')
+        self.object = planets[self.name]
 
-def get_position(location):
-    earth = planets['earth']
-    return earth + Topos(location.longitude, location.latitude)
-
-def print_ephemerides(obj, location, time):
-    object = planets[obj]
-    obj_name = obj.capitalize()
-    loc_name = location.name
-    loc_coords = get_position(location)
-    astrometric = get_astrometric(object, loc_coords, time)
-    alt, az, distance = astrometric.altaz()
-    tm = time.utc_datetime()
-    template = Template('Pozycja ${obj_name} dla ${loc_name} o czasie $tm: * Azymut: ${az} | * Wysoko ść: ${alt}').substitute(obj_name=obj_name, loc_name=loc_name, tm=tm, az=az, alt=alt)
-    print(template)
+    def observe(self, location, time):
+        astrometric = location.position.at(time.astrometric_dt).observe(self.object).apparent()
+        obj_name = self.caption
+        loc_name = location.name
+        alt, az, distance = astrometric.altaz()
+        tm = time.local_dt
+        observation = Template(
+            'Obserwuję ${obj_name} w miejscowości ${loc_name} o czasie $tm: * Azymut: ${az} | * Wysokość: ${alt}').substitute(
+            obj_name=obj_name, loc_name=loc_name, tm=tm, az=az, alt=alt)
+        print(observation)
 
 class Location:
     def __init__(self, name, longitude, latitude):
         self.name = name
         self.longitude = longitude
         self.latitude = latitude
+        self.set_position()
 
-planet = 'venus'
-ldz = Location('Łódź', '51.0 N', '19.0 W')
-# alx = Location('Aleksandrów Łódzki', +51.8146634, +19.2659747)
-# lodz_location = get_lodz_position()
-lodz_time = get_lodz_time()
-# print_ephemerides(planet, lodz_location, lodz_time)
-# print_ephemerides(planet, alx, lodz_time)
-print_ephemerides(planet, ldz, lodz_time)
+    def set_position(self):
+        planets = load('de421.bsp')
+        earth = planets['earth']
+        self.position = earth + Topos(self.longitude, self.latitude)
 
-observables = ['sun', 'moon', 'mercury', 'venus', 'mars', 'jupiter', 'saturn']
+class DateTime:
+    def __init__(self, js_timestamp):
+        self.timestamp = datetime.fromtimestamp(js_timestamp)
+        self.set_time()
 
+    def set_time(self):
+        timescale = load.timescale()
+        central = timezone('Europe/Warsaw')
+        d = self.timestamp
+        c = central.localize(d)
+        self.astrometric_dt = timescale.utc(c)
+        self.universal_dt = self.astrometric_dt.utc_datetime()
+        self.local_dt = self.astrometric_dt.astimezone(central)
+
+def perform_observations():
+    for observable in observables:
+        object = Celestial(observable)
+        object.observe(place, time)
+
+#-----INPUT----------------------------------
+#1. Coords
+place = Location('Łódź', '51.0 N', '19.0 W')
+#2. Time/Time range
+time = DateTime(1585254836)
+#3. Celestial objects
+observables = ['sun', 'moon', 'mercury', 'venus', 'mars']
+#-----MIDDLEWARE----------------------------------
+# -cardinal world direction enum
+# -from decimal coords to semi-decimal
+# -get other planets
+#-----OUTPUT----------------------------------
+perform_observations()
+#---------------------------------------------
